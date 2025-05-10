84c8b32e30d3b6940cf81130229474ded74aad66
hoangmanhkhiem <khiemhm04@gmail.com>
2025-05-11 03:08:02 +0700

    up

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+        dangerouslyAllowSVG: true,
+        contentSecurityPolicy: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; img-src 'self' data: https://example.com; style-src 'self' 'unsafe-inline';",
+  },
   // Thêm cấu hình khác nếu cần
 };
 
